0dd1b332034462c6685c463d8c29c27ac9dcfa71
spongebobpants <delay516@gmail.com>
2021-11-15 14:24:30 +0900

    update

---
+++ src/project/Ex01.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Ex01 {
+	public static void main(String[]argue) {
+
+		int countOfLettersInString =2;
+		System.out.println(countOfLettersInString);
+		int _person=3;
+		System.out.println(_person);
+		
+	}
+}
